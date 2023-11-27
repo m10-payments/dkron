0ae4b475d437a969a8e676f75815bcaa16d23def
Ivan Kripakov <ivan.kripakov@pashapay.az>
2023-11-27 14:55:40 +0400

    (WIP - consul like approach) use helm upgrade --install to upgrade existing dkron cluster

--- build-and-deploy-to-minikube.sh
+++ build-and-deploy-to-minikube.sh
@@ -1,4 +1,4 @@
 # shellcheck disable=SC2046
 eval $(minikube -p minikube docker-env)
 docker build -t m10-payments-dkron .
-helm install dkron --namespace dkron --create-namespace ./helm/dkron
+helm upgrade --install dkron --namespace dkron --create-namespace ./helm/dkron
